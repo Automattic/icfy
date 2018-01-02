c01176119b4cf82815e25f2ca3999a61d81d58dc
Jarda Snajdr <jsnajdr@gmail.com>
2018-01-02 13:52:38 +0100

    Add few new methods (insertPush, getBranches, ...) to API client

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-import { get } from 'axios';
+import { get, post } from 'axios';
 
 // const apiURL = 'http://localhost:5000';
 const apiURL = 'http://api.iscalypsofastyet.com:5000';
@@ -6,4 +6,7 @@ const apiURL = 'http://api.iscalypsofastyet.com:5000';
 export const getChartData = (chunk, period) => get(`${apiURL}/chart/${period}/${chunk}`);
 export const getChunkList = () => get(`${apiURL}/chunks`);
 export const getPush = sha => get(`${apiURL}/push/${sha}`);
+export const insertPush = push => post(`${apiURL}/push`, push);
 export const getDelta = (size, sha, prevSha) => get(`${apiURL}/delta/${size}/${prevSha}/${sha}`);
+export const getBranches = () => get(`${apiURL}/branches`);
+export const getBranch = branch => get(`${apiURL}/branch?branch=${branch}`);
