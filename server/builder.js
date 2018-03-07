f8c96e01a28fbc4cb2000f3c5bd8266d9aca9949
Jake F - @samouri <samouri@users.noreply.github.com>
2018-03-06 23:59:01 -0800

    Add support for new env variables that must be set for async components in babel 7 configuration

--- server/builder.js
+++ server/builder.js
@@ -73,7 +73,7 @@ async function processPush(push) {
 
 	// run the JS build in webpack analyze mode
 	await cmd(
-		'npm run -s env -- node --max_old_space_size=8192 ./node_modules/.bin/webpack --config webpack.config.js --profile --json > stats.json',
+		'CALYPSO_CLIENT=true npm run -s env -- node --max_old_space_size=8192 ./node_modules/.bin/webpack --config webpack.config.js --profile --json > stats.json',
 		{
 			useShell: true,
 			env: { NODE_ENV: 'production' },
