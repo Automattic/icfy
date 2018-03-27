8731452b1f876398c0c2c815802c66543f9ede17
Jarda Snajdr <jsnajdr@gmail.com>
2018-03-27 22:54:59 +0200

    Fix the unique columns in chunk_groups table

--- server/migrations/20180327125959_add_chunk_groups_table.js
+++ server/migrations/20180327125959_add_chunk_groups_table.js
@@ -1,12 +1,13 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('chunk_groups', t => {
-		t.string('sha', 160).notNullable().unique();
-		t.text('chunk').notNullable();
-		t.text('sibling').notNullable();
+		t.string('sha', 160).notNullable();
+		t.string('chunk', 160).notNullable();
+		t.string('sibling', 160).notNullable();
+		t.index(['sha', 'chunk']);
+		t.unique(['sha', 'chunk', 'sibling']);
 	});
 };
 
 exports.down = function(knex, Promise) {
 	return knex.schema.dropTable('chunk_groups');
 };
-
