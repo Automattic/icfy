ec02592b07b8247a1292d33f3d002096d1500b29
Jarda Snajdr <jsnajdr@gmail.com>
2019-03-08 10:24:37 +0100

    ChartView: show params in query URL, reorg state management

--- client/src/ChartView.js
+++ client/src/ChartView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { getChartData, getChunkList } from './api';
+import { buildQuery, getChartData, getChunkList } from './api';
 import Masterbar from './Masterbar';
 import Chart from './Chart';
 import PushDetails from './PushDetails';
@@ -16,6 +16,16 @@ const PERIODS = [
 	{ value: 'last1600', name: 'last 1600 pushes' },
 ];
 
+const DEFAULT_CHUNKS = 'build';
+const DEFAULT_SIZE = 'gzip_size';
+const DEFAULT_PERIOD = 'last200';
+const DEFAULT_BRANCH = 'master';
+
+const chunksToString = chunks => (chunks ? chunks.join(',') : DEFAULT_CHUNKS);
+const chunksFromString = chunks => (chunks || DEFAULT_CHUNKS).split(',');
+
+const nondefault = (value, defaultValue) => (value !== defaultValue ? value : undefined);
+
 function fillChartData(data, length) {
 	const missing = length - data.length;
 	if (missing > 0) {
@@ -25,10 +35,7 @@ function fillChartData(data, length) {
 }
 
 function mergeChartData(data, selectedSize) {
-	const maxLength = data.reduce((max, chunkData) => {
-		const length = chunkData.data.length;
-		return length > max ? length : max;
-	}, 0);
+	const maxLength = data.reduce((max, chunkData) => Math.max(max, chunkData.data.length), 0);
 
 	return data.map(chunkData => {
 		const data = fillChartData(chunkData.data.map(d => d[selectedSize]), maxLength);
@@ -41,13 +48,16 @@ class ChartView extends React.Component {
 		super(props);
 
 		const searchParams = new URLSearchParams(props.location.search);
-		const selectedBranch = searchParams.get('branch') || 'master';
+		const selectedChunks = searchParams.get('chunks') || DEFAULT_CHUNKS;
+		const selectedBranch = searchParams.get('branch') || DEFAULT_BRANCH;
+		const selectedSize = searchParams.get('size') || DEFAULT_SIZE;
+		const selectedPeriod = searchParams.get('period') || DEFAULT_PERIOD;
 
 		this.state = {
 			chunks: null,
-			selectedChunks: ['build'],
-			selectedSize: 'gzip_size',
-			selectedPeriod: 'last200',
+			selectedChunks,
+			selectedSize,
+			selectedPeriod,
 			selectedBranch,
 			data: null,
 			chartData: null,
@@ -61,13 +71,33 @@ class ChartView extends React.Component {
 		this.loadChart();
 	}
 
-	changeChunks = chunks => this.setChunks(chunks);
+	componentDidUpdate(prevProps, prevState) {
+		const chunksChanged = prevState.selectedChunks !== this.state.selectedChunks;
+		const periodChanged = prevState.selectedPeriod !== this.state.selectedPeriod;
+		const branchChanged = prevState.selectedBranch !== this.state.selectedBranch;
+		const sizeChanged = prevState.selectedSize !== this.state.selectedSize;
+
+		if (chunksChanged || periodChanged || branchChanged) {
+			this.loadChart();
+		}
+
+		if (chunksChanged || periodChanged || branchChanged || sizeChanged) {
+			this.updateSearchQuery();
+		}
+	}
+
+	changeChunks = chunks => this.setChunks(chunksToString(chunks));
 	changeSize = event => this.setSize(event.target.value);
 	changePeriod = event => this.setPeriod(event.target.value);
 
 	showPush = pushIndex => {
-		const pushToLoad = this.state.data[0].data[pushIndex];
-		const prevPush = pushIndex > 0 ? this.state.data[0].data[pushIndex - 1] : null;
+		// find the longest data array, in case they have unequal length
+		// (e.g., some chunk doesn't have the complete dataset because it's new)
+		const longestData = this.state.data.reduce((longest, data) =>
+			data.data.length > longest.data.length ? data : longest
+		);
+		const pushToLoad = longestData.data[pushIndex];
+		const prevPush = pushIndex > 0 ? longestData.data[pushIndex - 1] : null;
 
 		this.setState({
 			currentPushSha: pushToLoad.sha,
@@ -75,51 +105,48 @@ class ChartView extends React.Component {
 		});
 	};
 
-	loadChunks() {
-		getChunkList().then(response => {
-			const { chunks } = response.data;
-			this.setState({ chunks });
-		});
+	async loadChunks() {
+		const response = await getChunkList();
+		const { chunks } = response.data;
+		this.setState({ chunks });
 	}
 
-	loadChart() {
-		Promise.all(
-			this.state.selectedChunks.map(chunk =>
-				getChartData(chunk, this.state.selectedPeriod, this.state.selectedBranch).then(
-					response => ({
-						chunk,
-						data: response.data.data,
-					})
-				)
-			)
-		).then(data => this.setData(data));
+	async loadChart() {
+		const { selectedChunks, selectedPeriod, selectedBranch } = this.state;
+		const requests = chunksFromString(selectedChunks).map(async chunk => {
+			const response = await getChartData(chunk, selectedPeriod, selectedBranch);
+			return { chunk, data: response.data.data };
+		});
+		const data = await Promise.all(requests);
+		this.setData(data);
+	}
+
+	updateSearchQuery() {
+		const search = buildQuery({
+			chunks: nondefault(this.state.selectedChunks, DEFAULT_CHUNKS),
+			branch: nondefault(this.state.selectedBranch, DEFAULT_BRANCH),
+			size: nondefault(this.state.selectedSize, DEFAULT_SIZE),
+			period: nondefault(this.state.selectedPeriod, DEFAULT_PERIOD),
+		});
+		this.props.history.push({ search });
 	}
 
 	setChunks(selectedChunks) {
-		if (selectedChunks.length === 0) {
-			selectedChunks = ['build'];
-		}
-		this.setState({ selectedChunks }, () => this.loadChart());
+		this.setState({ selectedChunks });
 	}
 
 	setPeriod(selectedPeriod) {
-		this.setState({ selectedPeriod }, () => this.loadChart());
+		this.setState({ selectedPeriod });
 	}
 
 	setSize(selectedSize) {
-		const { data } = this.state;
-		this.setState({
-			selectedSize,
-			chartData: mergeChartData(data, selectedSize),
-		});
+		const chartData = mergeChartData(this.state.data, selectedSize);
+		this.setState({ selectedSize, chartData });
 	}
 
 	setData(data) {
-		const { selectedSize } = this.state;
-		this.setState({
-			data,
-			chartData: mergeChartData(data, selectedSize),
-		});
+		const chartData = mergeChartData(data, this.state.selectedSize);
+		this.setState({ data, chartData });
 	}
 
 	render() {
@@ -134,7 +161,7 @@ class ChartView extends React.Component {
 					<p>
 						Select the chunks to display:
 						<ChunkList
-							value={this.state.selectedChunks}
+							value={chunksFromString(this.state.selectedChunks)}
 							onChange={this.changeChunks}
 							options={this.state.chunks}
 						/>
