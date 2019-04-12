ce84a304f980ebae82e1e4ce06a6df2d5653df68
Jarda Snajdr <jsnajdr@gmail.com>
2019-04-12 11:02:43 +0200

    Increase the JSON body size limit from default 100kb to 2mb

--- server/api.js
+++ server/api.js
@@ -8,7 +8,7 @@ const port = 5000;
 const app = express();
 
 app.use(cors);
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '2mb' }));
 
 // API for frontend app
 app.get('/chunks', getChunks);
