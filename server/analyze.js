3edc0af4b35c6054462c751012f63e360e6c46f3
Jarda Snajdr <jsnajdr@gmail.com>
2020-02-28 08:37:25 +0100

    Remove old code for webpack 3 and legacy stats.json files

--- server/analyze.js
+++ server/analyze.js
@@ -14,37 +14,17 @@ function analyzeBundle(sha, stats, chart) {
 		};
 	});
 
-	const webpackMajorVersion = parseInt(stats.version, 10);
-
 	const chunkNames = _.fromPairs(_.map(stats.chunks, chunk => [chunk.id, chunk.names]));
 
 	const getName = id => _.get(chunkNames, [id, 0], id);
 
-	let chunkGroups;
-	if (webpackMajorVersion < 4) {
-		// support for webpack < 4
-		chunkGroups = [
-			{ sha, chunk: 'build', sibling: 'manifest' },
-			{ sha, chunk: 'build', sibling: 'vendor' },
-		];
-	} else if (!stats.namedChunkGroups) {
-		// old stats.json files that don't have `namedChunkGroups` yet
-		chunkGroups = _.flatMap(stats.chunks, chunk =>
-			chunk.siblings.map(sibling => ({
-				sha,
-				chunk: getName(chunk.id),
-				sibling: getName(sibling),
-			}))
-		);
-	} else {
-		chunkGroups = _.flatMap(stats.namedChunkGroups, ({ chunks }, groupName) =>
-			chunks.map(chunk => ({
-				sha,
-				chunk: groupName,
-				sibling: getName(chunk),
-			}))
-		);
-	}
+	const chunkGroups = _.flatMap(stats.namedChunkGroups, ({ chunks }, groupName) =>
+		chunks.map(chunk => ({
+			sha,
+			chunk: groupName,
+			sibling: getName(chunk),
+		}))
+	);
 
 	return { sha, chunkStats, chunkGroups, chunkNames };
 }
