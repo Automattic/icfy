8aeed28e1d4fa145505cc59fbad32a2a5f4bb772
Lance Willett <lance@automattic.com>
2026-05-30 10:18:01 -0700

    Fix critical dependency alerts (#69)
    Co-authored-by: Lance Willett <lance.willett@automattic.com>

--- server/scripts/stats-to-chart.js
+++ server/scripts/stats-to-chart.js
@@ -1,10 +1,13 @@
 const { writeFileSync } = require('fs');
 const { readStatsFromFile, getViewerData } = require('webpack-bundle-analyzer/lib/analyzer');
 
-function chartFromFile(filename) {
-	const stats = readStatsFromFile(filename);
+async function chartFromFile(filename) {
+	const stats = await readStatsFromFile(filename);
 	const chart = getViewerData(stats, './public/evergreen');
 	writeFileSync('client/chart.json', JSON.stringify(chart, null, 2));
 }
 
-chartFromFile('client/stats.json');
+chartFromFile('client/stats.json').catch(err => {
+	console.error(err);
+	process.exit(1);
+});
